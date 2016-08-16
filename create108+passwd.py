999f366e8d840a714a4917647877038df7575475
henrydobson <henrydobson@me.com>
2016-08-16 16:19:21 +0100

    parse arg and make output yaml format

--- create108+passwd.py
+++ create108+passwd.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import hashlib
 import binascii
-import os
+import os, sys
 
-password = b'password'
+password = sys.argv[0]
 salt = os.urandom(32)
 puppet_salt = binascii.hexlify(salt)
 iterations = 25000
@@ -11,6 +11,6 @@ iterations = 25000
 hex = hashlib.pbkdf2_hmac('sha512', password, salt, 25000, 128)
 puppet_password_hash = binascii.hexlify(hex)
 
-print puppet_password_hash
-print puppet_salt
-print iterations
+print "password:", puppet_password_hash
+print "salt:", puppet_salt
+print "iterations:", iterations
